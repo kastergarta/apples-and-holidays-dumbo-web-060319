d6252426d22efcfd83d33730dd459ecdd9d7e524
Dennis <dennis.basyrov@gmail.com>
2019-05-21 03:23:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,34 +18,52 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  return holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_supplies[:winter][:christmas] << "Balloons"
+holiday_supplies[:winter][:new_years] << "Balloons"
 end
+
+
+
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
+  
+holiday_supplies[:spring][:memorial_day] << supply
+return holiday_supplies
 
 end
+
+
+
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
 
+holiday_hash[season][holiday_name] = supply_array
+# holiday_supplies[:winter][:valentines_day] = ["Cupid Cut-Out", "Candy Hearts"]
+return holiday_supplies
 end
 
-def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
 
+def all_winter_holiday_supplies(holiday_supplies) 
+return  holiday_supplies.values.to_s 
 end
+
 
 def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, data|
+  puts "#{season}:"
+  puts "#{data}"
+  
+end
+  
+  
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
@@ -59,11 +77,25 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  
+  new_array = []
+holiday_hash.each do |season, data|
+  
+  
+  data.select { |m, n| n == "BBQ"  }
+    
+   
+      # new_array << m
+      return new_array
+    
 
 end
 
+# return new_array
+end
 
 
+[[], []]
 
 
 
